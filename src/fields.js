9f4e6f9e3076ac5759d42826d3b352767a1d163e
Nick Curry <cnick79@gmail.com>
2016-03-12 15:17:13 -0500

    Created available list of fields

---
+++ src/fields.js
@@ -0,0 +1,20 @@
+/*jshint node: true*/
+/*global require, module */
+
+/**
+ * Available fields for the CDX query.
+ */
+
+'use strict';
+
+var FIELDS = {
+        URLKEY: "urlkey",
+        TIMESTAMP: "timestamp",
+        ORIGINAL: "original",
+        MIMETYPE: "mimetype",
+        STATUSCODE: "statuscode",
+        DIGEST: "digest",
+        LENGTH: "length"
+    };
+
+module.exports = FIELDS;
